69ff5f71ccb579c7b60a20ffff2208f24fdfac44
Chris Metcalf <chris@chrismetcalf.net>
2014-06-20 09:47:58 -0700

    Adding a catalog because why not

--- js/foundry.js
+++ js/foundry.js
@@ -2,12 +2,29 @@ $(document).ready(function(){
   // Split up our hash components
   var components = window.location.hash.split("/");
   if(components.length <= 1) {
-    console.log("No parameters passed!");
+    $("#docs").html("<p>No parameters passed!</p>");
     return;
   } else if(components.length == 2) {
     // Catalog
-    $('#docs').html("<p>Put a catalog here...</p>");
+    $('#docs').html("<p>Loading results for " + components[1] + "...</p>");
+    $.when(
+      $.getJSON("https://" + components[1] + "/api/search/views.json?limitTo=TABLES")
+    )
+    .done(function(results) {
+      //  Load our docs from our metadata
+      $.get("/foundry/catalog.mst", function(template) {
+        $('#docs').html(Mustache.render(template, {
+          domain: components[1],
+          count: results.count,
+          datasets: results.results
+        }));
+      });
+    })
+    .fail(function() {
+      console.log("Something went wrong...");
+    });
   } else if(components.length == 3) {
+    $('#docs').html("<p>Loading docs for " + components[2] + "...</p>");
     // API
     // Parallellize our data and metadata requests
     $.when(
@@ -19,6 +36,7 @@ $(document).ready(function(){
       //{% capture full_url %}https://{{ include.domain }}{{ include.path }}?{{ include.args }}{% endcapture %}
       $.get("/foundry/template.mst", function(template) {
         $('#docs').html(Mustache.render(template, {
+          domain: components[1],
           metadata: metadata[0],
           row: data[0][0],
           full_url: function() { return "https://" + components[1] + "/resource/" + components[2] + ".json"; }
