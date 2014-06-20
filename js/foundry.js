aa1162b207f368a1e123e1355b124ab195edacb2
Chris Metcalf <chris@chrismetcalf.net>
2014-06-20 13:12:46 -0700

    Somewhat hokey branding

--- js/foundry.js
+++ js/foundry.js
@@ -4,17 +4,48 @@ $(document).ready(function(){
   if(components.length <= 1) {
     $("#docs").html("<p>No parameters passed!</p>");
     return;
-  } else if(components.length == 2) {
+  }
+
+  var domain = components[1];
+
+  // We've got at least one parameter, so we know we've at least got a domain
+  // Fetch config and do a little styling
+  $.getJSON("http://" + domain + "/api/configurations.json?type=site_theme")
+  .done(function(config) {
+    console.log(config);
+    var logo_slug = $.grep(config[0].properties, function(p, idx) { return p.name === "theme_v2b" })[0].value
+      .images.logo_header.href;
+
+    // If there's no logo configured for this domain
+    if(!logo_slug) {
+      console.log("No logo found for " + domain)
+    }
+
+    // Slap a logo in there
+    if(logo_slug.indexOf("/") >= 0) {
+      $("#branding").append('<img src="https://' + domain + logo_slug + '" alt="' + domain + '" />');
+    } else {
+      $("#branding").append('<img src="https://' + domain + '/api/assets/' + logo_slug + '" alt="' + domain + '" />');
+    }
+  })
+  .fail(function() {
+    console.log("Error getting configuration for domain " + domain);
+    return;
+  });
+
+
+  // Deal with generating the docs or catalog
+  if(components.length == 2) {
     // Catalog
-    $('#docs').html("<p>Loading results for " + components[1] + "...</p>");
+    $('#docs').html("<p>Loading results for " + domain + "...</p>");
     $.when(
-      $.getJSON("https://" + components[1] + "/api/search/views.json?limitTo=TABLES")
+      $.getJSON("https://" + domain + "/api/search/views.json?limitTo=TABLES")
     )
     .done(function(results) {
       //  Load our docs from our metadata
       $.get("/foundry/catalog.mst", function(template) {
         $('#docs').html(Mustache.render(template, {
-          domain: components[1],
+          domain: domain,
           count: results.count,
           datasets: results.results
         }));
@@ -28,18 +59,18 @@ $(document).ready(function(){
     // API
     // Parallellize our data and metadata requests
     $.when(
-      $.getJSON("https://" + components[1] + "/api/views/" + components[2] + ".json"),
-      $.getJSON("https://" + components[1] + "/resource/" + components[2] + ".json?$limit=1")
+      $.getJSON("https://" + domain + "/api/views/" + components[2] + ".json"),
+      $.getJSON("https://" + domain + "/resource/" + components[2] + ".json?$limit=1")
     )
     .done(function(metadata, data) {
       //  Load our docs from our metadata
       //{% capture full_url %}https://{{ include.domain }}{{ include.path }}?{{ include.args }}{% endcapture %}
       $.get("/foundry/template.mst", function(template) {
         $('#docs').html(Mustache.render(template, {
-          domain: components[1],
+          domain: domain,
           metadata: metadata[0],
           row: data[0][0],
-          full_url: function() { return "https://" + components[1] + "/resource/" + components[2] + ".json"; }
+          full_url: function() { return "https://" + domain + "/resource/" + components[2] + ".json"; }
         }));
       });
     })
