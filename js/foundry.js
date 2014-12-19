c39feee48bf67932a918bbaa1516c2e4716cf327
Chris Metcalf <chris@chrismetcalf.net>
2014-12-19 15:59:38 -0800

    Basic error handling for API Foundry metadata grabber

--- js/foundry.js
+++ js/foundry.js
@@ -8,74 +8,63 @@ $(document).ready(function(){
   var domain = components[1];
 
   // Deal with generating the docs or catalog
-  if(components.length == 2) {
-    // Catalog
-    $('#foundry-docs').html("<p>Loading results for " + domain + "...</p>");
-    $.when(
-      $.getJSON("https://" + domain + "/api/search/views.json?limitTo=TABLES")
-    )
-    .done(function(results) {
-      //  Load our docs from our metadata
-      $.get("/foundry/catalog.mst", function(template) {
-        $('#foundry-docs').html(Mustache.render(template, {
-          domain: domain,
-          count: results.count,
-          datasets: results.results
-        }));
-      });
-    })
-    .fail(function() {
-      console.log("Something went wrong...");
+  $('#foundry-docs').html("<p>Generating API documentation...</p>");
+  // Parallellize our data and metadata requests
+  $.when(
+    $.getJSON("https://" + domain + "/api/views/" + components[2] + ".json"),
+    $.getJSON("https://" + domain + "/resource/" + components[2] + ".json?$limit=1"),
+    $.getJSON("https://" + domain + "/resource/" + components[2] + ".json?$select=count(*)")
+  ).done(function(metadata, data, count) {
+    // Modify metadata to include trial URLs
+    $.each(metadata[0].columns, function(idx, col) {
+      col.filter_url = "https://" + domain + "/resource/" + metadata[0].id + ".json?"
+        + col.fieldName + "=" + data[0][0][col.fieldName];
     });
-  } else if(components.length == 3) {
-    $('#foundry-docs').html("<p>Loading docs for " + components[2] + "...</p>");
-    // API
-    // Parallellize our data and metadata requests
+
+    // Convert our timestamps into printable times
+    $.each(["createdAt", "rowsUpdatedAt"], function(idx, name){
+      metadata[0][name] = (new Date(metadata[0][name]*1000).toLocaleString());
+    });
+
+    // Update our page header
+    $("h1.title").html(metadata[0].name);
+    document.title = metadata[0].name + " | Socrata API Foundry";
+
+    //  Load our docs from our metadata
     $.when(
-      $.getJSON("https://" + domain + "/api/views/" + components[2] + ".json"),
-      $.getJSON("https://" + domain + "/resource/" + components[2] + ".json?$limit=1"),
-      $.getJSON("https://" + domain + "/resource/" + components[2] + ".json?$select=count(*)")
-    ).done(function(metadata, data, count) {
-      // Modify metadata to include trial URLs
-      $.each(metadata[0].columns, function(idx, col) {
-        col.filter_url = "https://" + domain + "/resource/" + metadata[0].id + ".json?"
-          + col.fieldName + "=" + data[0][0][col.fieldName];
-      });
+      $.ajax("/foundry/template.mst"),
+      $.ajax("/foundry/tryit.mst")
+    ).done(function(template, tryit) {
+      $('#foundry-docs').html(Mustache.render(template[0], {
+        domain: domain,
+        metadata: metadata[0],
+        row: data[0][0],
+        count: count[0][0].count.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"),
+        full_url: function() { return "https://" + domain + "/resource/" + components[2] + ".json"; },
+      }, {
+        tryit: tryit[0]
+      }));
 
-      // Convert our timestamps into printable times
-      $.each(["createdAt", "rowsUpdatedAt"], function(idx, name){
-        metadata[0][name] = (new Date(metadata[0][name]*1000).toLocaleString());
-      });
+      // Set up our livedocs links
+      setup_livedocs();
 
-      // Update our page header
-      $("h1.title").html(metadata[0].name);
-
-      //  Load our docs from our metadata
-      $.when(
-        $.ajax("/foundry/template.mst"),
-        $.ajax("/foundry/tryit.mst")
-      ).done(function(template, tryit) {
-        $('#foundry-docs').html(Mustache.render(template[0], {
-          domain: domain,
-          metadata: metadata[0],
-          row: data[0][0],
-          count: count[0][0].count.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"),
-          full_url: function() { return "https://" + domain + "/resource/" + components[2] + ".json"; },
-        }, {
-          tryit: tryit[0]
-        }));
-
-        // Set up our livedocs links
-        setup_livedocs();
-
-        // Set up our clipboard buttons
-        $.each($("pre"), clipbutton);
-      }).fail(function() {
-        console.log("Something went wrong fetching templates...");
-      });
+      // Set up our clipboard buttons
+      $.each($("pre"), clipbutton);
     }).fail(function() {
-      console.log("Something went wrong retrieving metadata...");
+      $('#foundry-docs').html("<p>Something went wrong fetching templates.</p>");
     });
-  }
+  }).fail(function(xhr) {
+    switch(xhr.status) {
+      case 403:
+        $('#foundry-docs').html("<p>Unfortunately, this dataset is private, and API Foundry does not support private datasets at this time.</p>");
+        break;
+      case 404:
+        $('#foundry-docs').html("<p>This dataset cannot be found or has been deleted.</p>");
+        break;
+      default:
+        $('#foundry-docs').html("<p>Something went wrong fetching metadata</p>");
+        break;
+    }
+  });;
 });
 
