2b0a1748c5ae37bd1626100f57ef25bfb4e60762
Chris Metcalf <chris@chrismetcalf.net>
2015-01-02 14:42:58 -0800

    Basic branding header that inserts itself above the rest of the Foundry content

--- js/foundry.js
+++ js/foundry.js
@@ -1,19 +1,79 @@
 $(document).ready(function(){
   // Split up our hash components
   var components = window.location.hash.split("/");
-  if(components.length <= 1) {
+  if(components.length != 3) {
     $("#foundry-docs").html("<p>No parameters passed!</p>");
     return;
   }
   var domain = components[1];
+  var uid = components[2];
+
+  // Fetch branding details
+  $.getJSON("https://" + domain + "/api/configurations.json?type=site_theme&defaultOnly=true&merge=true")
+    .done(function(data) {
+      // Grep out the stuff that we care about
+      var org_name = $.grep(data[0].properties, function(n) { return n["name"] == "strings.company" })[0].value;
+      var theme = $.grep(data[0].properties, function(n) { return n["name"] == "theme_v2b" })[0].value;
+
+      // Determine what kind of logo we have
+      var logo_href = null;
+      var logo_config = theme.images.logo_header;
+      if(logo_config != null && logo_config.href != null && logo_config.href.length > 0) {
+        var matches = logo_config.href.match(/^\w+-\w+-\w+-\w+-\w+$/);
+        if(logo_config.href != null && matches) {
+          // Looks like we have an asset tag
+          logo_href = "https://" + domain + "/api/assets/" + logo_config.href;
+
+        } else if(logo_config.href.match(/^http/)) {
+          // They appear to have provided a full URL
+          logo_href = logo_config.href;
+
+        } else if(logo_config.href != null) {
+          // Assume that this is just a relative path
+          logo_href = "https://" + domain + logo_config.href;
+        }
+
+        // Double check we're not using the default Socrata logo...
+        if(logo_href.match(/socrata_logo.png$/)) {
+          console.log("This site is still using the default Socrata logo, hiding the logo from branding.");
+          logo_href = null;
+        }
+      } else {
+        console.log("No header logo configuration provided for this domain.");
+      }
+
+      $.ajax("/foundry/branding.mst")
+        .done(function(template) {
+          $('#branding').html(Mustache.render(template, {
+            org_name: org_name,
+            logo_href: logo_href,
+            org_homepage: "http://" + domain + "/"
+          }));
+
+          // Clean up bad logos...
+          $("img.logo").error(function() {
+            console.log("Something went wrong loading logo image: " + $(this).attr("src"));
+            $(this).remove();
+          });
+        })
+        .fail(function(xhr) {
+          console.log("An error occurred loading the branding template:" + xhr); 
+        });
+
+
+    })
+    .fail(function(xhr) {
+      console.log("Something went wrong loading branding configuration: " + xhr);
+    });
+
 
   // Deal with generating the docs or catalog
   $('#foundry-docs').html("<p>Generating API documentation...</p>");
-  // Parallellize our data and metadata requests
+  // Parallelize our data and metadata requests
   $.when(
-    $.getJSON("https://" + domain + "/api/views/" + components[2] + ".json"),
-    $.getJSON("https://" + domain + "/resource/" + components[2] + ".json?$limit=1"),
-    $.getJSON("https://" + domain + "/resource/" + components[2] + ".json?$select=count(*)")
+    $.getJSON("https://" + domain + "/api/views/" + uid + ".json"),
+    $.getJSON("https://" + domain + "/resource/" + uid + ".json?$limit=1"),
+    $.getJSON("https://" + domain + "/resource/" + uid + ".json?$select=count(*)")
   ).done(function(metadata, data, count) {
     // Modify metadata to include trial URLs
     $.each(metadata[0].columns, function(idx, col) {
@@ -40,7 +100,7 @@ $(document).ready(function(){
         metadata: metadata[0],
         row: data[0][0],
         count: count[0][0].count.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"),
-        full_url: function() { return "https://" + domain + "/resource/" + components[2] + ".json"; },
+        full_url: function() { return "https://" + domain + "/resource/" + uid + ".json"; },
       }, {
         tryit: tryit[0]
       }));
