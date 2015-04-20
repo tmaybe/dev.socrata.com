b08bea480303fe01825d03f2b3162a0d0599b8c2
Chris Metcalf <chris@chrismetcalf.net>
2015-04-19 22:25:00 -0700

    Working pretty solidly with at least my single test dataset now.

--- js/foundry.js
+++ js/foundry.js
@@ -16,15 +16,115 @@ var dataset = function(domain, uid) {
 
   // Parallelize our data and metadata requests
   $.when(
-    $.getJSON("https://" + domain + "/api/views.json?method=getByResourceName&name=" + uid),
-    $.getJSON("https://" + domain + "/resource/" + uid + ".json?$limit=1"),
-    $.getJSON("https://" + domain + "/resource/" + uid + ".json?$select=count(*)")
-  ).done(function(metadata, data, count) {
-    // Modify metadata to include trial URLs
+    $.getJSON("https://" + domain + "/api/views.json?method=getByResourceName&name=" + uid), // Metadata
+    $.getJSON("https://" + domain + "/api/migrations/" + uid + ".json"), // Migrations API
+    $.getJSON("https://" + domain + "/resource/" + uid + ".json?$limit=1"), // Sample data row
+    $.getJSON("https://" + domain + "/resource/" + uid + ".json?$select=count(*)") // Count
+  ).done(function(metadata, migration, data, count) {
+    // We got migration details, let's parse the mapping too
+    var migration_mapping = null;
+    var nbe_uid = null;
+    var obe_uid = null;
+    var last_synced = 0;
+    var is_obe = true;
+
+    if(migration[1] == "success") {
+      migration_mapping = JSON.parse(migration[0].controlMapping);
+      nbe_uid = migration[0].nbeId;
+      obe_uid = migration[0].obeId;
+      last_synced = (new Date(migration[0].syncedAt*1000).toLocaleString());
+      is_obe = (obe_uid == uid);
+    }
+
+    // Modify column metadata
     $.each(metadata[0].columns, function(idx, col) {
-      col.filter_url = "https://" + domain + "/resource/" + uid + ".json?"
-        + col.fieldName + "=" + data[0][0][col.fieldName];
+      // Include trial URLs
+      var val = data[0][0][col.fieldName];
+
+      // Custom trial URLs for rich datatypes
+      switch(col.dataTypeName) {
+        case "text":
+          col.filter = col.fieldName + "=" + val;
+          break;
+
+        case "number":
+          col.filter = col.fieldName + "=" + val;
+          col.query = col.fieldName + " > " + val;
+          break;
+
+        case "money":
+          col.filter = col.fieldName + "=" + val;
+          col.query = col.fieldName + " < " + val;
+          break;
+
+        case "calendar_date":
+          col.filter = col.fieldName + "=" + val;
+          col.query = col.fieldName + " <= '" + val + "'";
+          break;
+
+        case "checkbox":
+          col.filter = col.fieldName + "=" + val;
+          break;
+
+        case "point":
+          col.query = "within_circle("
+            + col.fieldName + ", "
+            + Math.round(val.coordinates[1]*100)/100 + ", "
+            + Math.round(val.coordinates[0]*100)/100 + ", 1000)";
+          break;
+
+        case "location":
+          col.query = "within_circle("
+            + col.fieldName + ", "
+            + Math.round(val.latitude*100)/100 + ", "
+            + Math.round(val.longitude*100)/100 + ", 1000)";
+          break;
+
+        case "document":
+          // Nom nom nom
+          break;
+
+        case "photo":
+          // Nom nom nom
+          break;
+
+        case "email":
+          // Nom nom nom
+          break;
+
+        case "url":
+          // Nom nom nom
+          break;
+
+        default:
+          col.filter = col.fieldName + "=" + val;
+          break;
+
+      };
+
+      if(migration_mapping) {
+        // Column mapping
+        col.equivalent = migration_mapping.nameShortening[col.fieldName];
+        col.flattening = migration_mapping.flatten[col.fieldName];
+      }
     });
+
+    // Roll up our changes so we can use them in our mustache template
+    var splits = _.collect(migration_mapping.flatten, function(mapping, key) {
+      return {
+        from_col: key,
+        to_cols: _.collect(mapping, function(val, key) {
+          return "<code>" + val.fieldname + "</code>";
+        }).join(", ")
+      };
+    });
+    var renames = _.chain(migration_mapping.nameShortening)
+      .collect(function(to, from) {
+        return { from: from, to: to };
+      })
+      .filter(function(obj) {
+        return obj.from != obj.to;
+      }).value();
 
     // Convert our timestamps into printable times
     $.each(["createdAt", "rowsUpdatedAt"], function(idx, name){
@@ -44,6 +144,16 @@ var dataset = function(domain, uid) {
         uid: uid,
         domain: domain,
         metadata: metadata[0],
+        nbe_uid: nbe_uid,
+        obe_uid: obe_uid,
+        migration_mapping: migration_mapping,
+        is_obe: is_obe,
+        show_migration: flags.show_migration && is_obe,
+        has_structural_changes: splits.length > 0,
+        splits: splits,
+        has_renames: renames.length > 0,
+        renames: renames,
+        last_synced: last_synced,
         row: data[0][0],
         count: count[0][0].count.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"),
         full_url: function() { return "https://" + domain + "/resource/" + uid + ".json"; },
@@ -71,7 +181,6 @@ var dataset = function(domain, uid) {
             .addClass("fa-plus-square-o");
         });
       });
-
 
       // Show ourselves!
       $("#loading").fadeOut();
@@ -104,7 +213,7 @@ var dataset = function(domain, uid) {
   });;
 };
 
-$(document).ready(function(){
+var load = function() {
   // Throw up a loading screen while we work
   $('#branding').hide();
   $('#foundry-docs').hide();
@@ -125,4 +234,11 @@ $(document).ready(function(){
     $("#foundry-docs").html("<p>No parameters passed!</p>");
     return;
   }
+}
+
+// Initial load
+$(document).ready(function() {
+  load();
+  window.onhashchange = load();
 });
+
