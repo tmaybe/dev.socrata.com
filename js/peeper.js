b53facf45c1f75afd900b0b84944c413bcfa49c7
Chris Metcalf <chris@chrismetcalf.net>
2015-06-06 00:14:16 -0700

    Adding Peeper, your Snuffie friend who definitely isn't Clippy

---
+++ js/peeper.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  if(Cookies.get('hide-peeper')) {
+    console.log("Peeper has been hidden"); 
+    return;
+  }
+
+  var peeper = '<div id="peeper">'
+    + '<a href="/support.html">'
+    + '<img src="/img/peeper.png" alt="Need a hand?" />'
+    + '</a>'
+    + '<a href="#" class="dismiss"><i class="fa fa-times"></i> go away</a>'
+    + '</div>';
+  $("body").append(peeper);
+
+  $('#peeper .dismiss').click(function() {
+    $('#peeper').animate({
+      right: "-270px"   
+    }, 500);
+    Cookies.set('hide-peeper', true, { expires: 3 });
+  });
+
+  setTimeout(function() {
+    $("#peeper").animate({
+      right: "-120px"
+    }, 1000);
+  }, 4000);
+});
+
