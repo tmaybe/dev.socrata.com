3ed0e92ceda15cbeb7aa84c78320233668643ef6
Chris Metcalf <chris@chrismetcalf.net>
2016-03-17 15:32:40 -0700

    Adding longer timeouts & debugging

--- Makefile
+++ Makefile
@@ -16,7 +16,10 @@ htmlproof:
 	bundle exec htmlproof ./public --only-4xx --check-html --href-ignore "/#/,/\/foundry/,/\/register/,/APP_TOKEN/"
 
 casperjs:
-	casperjs test _tests/*
+	casperjs --verbose --log-level=warning test _tests/*
+
+casperdebug:
+	casperjs --verbose --log-level=debug test _tests/*
 
 # Generates a build stamp and plugs it into a file in public
 SHA=$(shell git rev-parse --short HEAD)
--- _tests/foundry.js
+++ _tests/foundry.js
@@ -1,6 +1,10 @@
 // Tests various API Foundry use cases
 var base = 'https://dev.socrata.com';
 
+var timeout = function(delay) {
+  casper.log('Timeout after ' + delay + ' milliseconds.', 'warning');
+};
+
 /// 2.1 API, no redirect
 casper.test.begin("2.1 API, no redirect", 7, function(test) {
   casper.start(base + '/foundry/soda.demo.socrata.com/b6kv-3wgw');
@@ -10,21 +14,21 @@ casper.test.begin("2.1 API, no redirect", 7, function(test) {
     test.assertSelectorHasText('#branding h4.welcome', 
         'Socrata API, powered by Socrata', 
         'branding header is present and correct');
-  });
+  }, timeout, 10000);
 
   // Check splash
   casper.waitForSelectorTextChange('#splash', function() {
     test.assertSelectorHasText('#splash .alert', 
         'You\'re already using the latest version of this dataset API',
         'splash states we\'re on the latest version');
-  });
+  }, timeout, 10000);
 
   // Check title
   casper.waitForSelectorTextChange('h1#title', function() {
     test.assertSelectorHasText('h1#title',
         'Old Backend Dataset Name',
         'foundry is using old endpoint title');
-  });
+  }, timeout, 10000);
 
   // Check Foundry itself
   casper.waitForSelectorTextChange('#foundry-docs', function() {
@@ -41,7 +45,7 @@ casper.test.begin("2.1 API, no redirect", 7, function(test) {
     test.assertSelectorHasText('#foundry-docs .endpoint .target',
         '/resource/b6kv-3wgw.json',
         'resource endpoint is correct');
-  });
+  }, timeout, 10000);
 
   casper.run(function() {
     test.done();
